61b6ae9f1b7c664ad9491c0e2c0ef0657b13530a
shubhamsapra97 <shubham.sapra97@gmail.com>
2017-03-17 17:17:01 +0530

    Rectangle Area.cpp added

---
+++ Algorithm C++/Rectangle Area/Rectangle Area.cpp
@@ -0,0 +1,24 @@
+
+/*
+ * Create classes Rectangle and RectangleArea
+ */
+
+class Rectangle{
+    public:
+        int height,width;
+        void display(){
+            cout<<width<<" "<<height<<"\n";
+        }
+};
+
+class RectangleArea : public Rectangle{
+    public:
+        void read_input(){
+            cin>>width>>height;
+        }
+    
+        void display(){
+            cout<<(width*height);
+        }
+};
+
