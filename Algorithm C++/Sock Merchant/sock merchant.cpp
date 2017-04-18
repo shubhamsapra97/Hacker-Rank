842f3ddc0f70574eb74f9fb32cfe8d88b798fde1
shubhamsapra97 <shubham.sapra97@gmail.com>
2017-04-18 22:11:29 +0530

    Sock Merchant.cpp added

--- Algorithm C++/Sock Merchant/sock merchant.cpp
+++ Algorithm C++/Sock Merchant/sock merchant.cpp
@@ -1,26 +1,60 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
 #include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
 using namespace std;
+
+
 int main(){
-    int n,count=0;
-    int p=789456;
-    cin>>n;
-    int c[n];
-    for(int i=0;i<n;i++){
-       cin>>c[i];
+    int n,h=0,k;
+    cin >> n;
+    vector<int> c(n);
+    for(int i = 0;i < n;i++){
+       cin >> c[i];
     }
-    for(int i=0;i<n-1;i++)
-        {
-        for(int j=i+1;j<n;j++)
-            {
-            if(c[i]==c[j])
-                {
-                count++;
-                c[j]=p;
-                p++;
-                break;
-            }
+    for(int i = 0;i < n;i++){
+        k=0;
+        for(int g=i+1;g<n;g++){
+   
+       if(i!=0){
+           
+           for(int j=0;j<i;j++){
+               if(c[i]==c[j]){
+                   goto m;
+               }
+           }
+           
+       }
+        
+        if(c[g]==c[i]){
+            k++;     
         }
+      }       
+        k++;
+        h += k/2;
+        
+        m:0;
     }
-    cout<<count;
+    cout<<h;
     return 0;
 }
+
