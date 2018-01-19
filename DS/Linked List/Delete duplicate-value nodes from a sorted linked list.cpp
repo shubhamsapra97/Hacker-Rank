7d912c673dcfd3726fc2092797e9ab300b32e222
shubhamsapra97 <shubham.sapra97@gmail.com>
2018-01-20 00:31:04 +0530

    Delete duplicate-value nodes from a sorted linked list added

---
+++ DS/Linked List/Delete duplicate-value nodes from a sorted linked list.cpp
@@ -0,0 +1,26 @@
+/*
+  Remove all duplicate elements from a sorted linked list
+  Node is defined as
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* RemoveDuplicates(Node *head)
+{
+    struct Node *ptr = head;
+    if(head == NULL){
+        return NULL;
+    }
+    x:1;
+    while(ptr->next != NULL){
+        //cout<<"CURRENT"<<ptr->data<<"\nNEXT"<<ptr->next->data;
+        if(ptr->data == ptr->next->data){
+            ptr->next = ptr->next->next;
+            goto x;
+        }
+        ptr=ptr->next;
+    }
+    return head;
+}
